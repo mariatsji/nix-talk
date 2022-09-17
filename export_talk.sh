32f2df3a590f083386ab8cb7ded9ed298508bcdf
Sjur Millidahl <smillida@cisco.com>
2022-09-17 07:32:59 +0200

    refactor nix

--- export_talk.sh
+++ export_talk.sh
@@ -1,1 +1,4 @@
-nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/ee01de29d2f58d56b1be4ae24c24bd91c5380cea.tar.gz -p pandoc wkhtmltopdf-bin --run 'pandoc talk.md --pdf-engine=wkhtmltopdf -o talk.pdf'
+nix-shell --pure \
+  -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/ee01de29d2f58d56b1be4ae24c24bd91c5380cea.tar.gz \
+  -p pandoc wkhtmltopdf-bin \
+  --run 'pandoc talk.md --pdf-engine=wkhtmltopdf -o talk.pdf'
