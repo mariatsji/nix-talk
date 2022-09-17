9adff2478df16eaf3c4c5189da665a58d45af6c3
Sjur Millidahl <smillida@cisco.com>
2022-09-17 07:28:56 +0200

    export talk as pdf

---
+++ export_talk.sh
@@ -0,0 +1,1 @@
+nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/ee01de29d2f58d56b1be4ae24c24bd91c5380cea.tar.gz -p pandoc wkhtmltopdf-bin --run 'pandoc talk.md --pdf-engine=wkhtmltopdf -o talk.pdf'
