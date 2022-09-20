77edf61053e619da2c166422b5b435d29c4f342c
Sjur Millidahl <smillida@cisco.com>
2022-09-20 09:36:34 +0200

    rename talk.pdf

--- export_talk.sh
+++ export_talk.sh
@@ -1,4 +1,4 @@
 nix-shell --pure \
   -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/ee01de29d2f58d56b1be4ae24c24bd91c5380cea.tar.gz \
   -p pandoc wkhtmltopdf-bin fira-mono \
-  --run 'pandoc talk.md --pdf-engine=wkhtmltopdf --wrap=none -o talk.pdf'
+  --run 'pandoc talk.md --pdf-engine=wkhtmltopdf --wrap=none -o 2022_nix_introduction.pdf'
